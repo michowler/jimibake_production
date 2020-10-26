71cfb715251a42c88cb921e130ff21577640ecd1
michowler <mlhy9778@hotmail.com>
2020-10-26 22:58:44 +0800

    change menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'homepage#home', as: "root"
+  get '/menu', to: 'homepage#menu'
+  get '/about', to: 'homepage#about'
+  get '/daily', to: 'homepage#daily'
   
 end
